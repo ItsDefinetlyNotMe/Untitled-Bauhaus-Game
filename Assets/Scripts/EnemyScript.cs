85876c7ef1bf9dcb09f69590e0e14ec4e9c2a92d
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-04-29 02:12:37 +0200

    Added A* and experimented with Pathfinding

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,22 +1,56 @@
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyScript : MonoBehaviour
 {
-    int maxHealth = 100;
-    int currentHealth;
-    float size = 1f;
     Rigidbody2D rb;
-    GameObject player;
+    //AI 
+    [SerializeField] private Transform target;
+    private float baseMovementspeed = 1f;
+    private float nextWaypointDistance = .2f;
+    int currentWaypoint = 0;
+    private bool isThere = false;
+    Path path;
+    Seeker seeker;
     WeaponScript weaponScript;
-
 
     void Start()
     {
-        currentHealth = maxHealth;
+        seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Mario"); //TODO Mario won't be Mario forever
-        weaponScript = GameObject.Find("Mario/Sword").GetComponent<WeaponScript>(); //TODO Don't have damage number in weapon script please (Maybe in GameManager?)
-        
+
+        InvokeRepeating("UpdatePath",0f,.5f);
+    }
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+            seeker.StartPath(rb.position, target.position,OnPathComplete);        
+            
+    }
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        if(path == null)
+            return;
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            isThere = true;
+        }else{
+            isThere = false;
+        }
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = rb.velocity.normalized  + (direction * baseMovementspeed).normalized;
+        rb.velocity = force.normalized;
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance)
+            ++currentWaypoint;
     }
     //pathfinding etc general behaviour
 }
