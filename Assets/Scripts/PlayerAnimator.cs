7b5012d078b63bcb221caceb684af9926c0ac092
Raphael <50047718+Nordwindfeld@users.noreply.github.com>
2023-05-02 12:55:19 +0200

    Change script for fotsteep (but still not perfect)

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,7 +5,7 @@ public class PlayerAnimator : MonoBehaviour
 {
     private Animator animator;
     private Vector2 movementDirection;
-
+    public GameObject Sound;
 
     private void Awake()
     {
@@ -21,13 +21,23 @@ public class PlayerAnimator : MonoBehaviour
             animator.SetBool("isWalking", true);
             animator.SetFloat("X", movementDirection.x);
             animator.SetFloat("Y", movementDirection.y);
+            InvokeRepeating("PlayFootStepSound", 0.3f, 0.5f);
         }
 
         else
+        {
             animator.SetBool("isWalking", false);
+            CancelInvoke("PlayFootStepSound");
+        }
     }
     public void PlayAttackAnimation(string attackDirection)
     {
         animator.Play(attackDirection);
+    }
+
+
+    private void PlayFootStepSound()
+    {
+        Sound.GetComponent<RandomSound>().PlayRandom1();
     }
 }
