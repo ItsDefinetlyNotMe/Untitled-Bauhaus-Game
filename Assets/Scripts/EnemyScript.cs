f756466788cb07f93c920c99e5f6ed27aa315e3f
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-05-02 22:00:37 +0200

    Add A* basics with Chasing Enemy

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,22 +1,77 @@
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyScript : MonoBehaviour
 {
-    int maxHealth = 100;
-    int currentHealth;
-    float size = 1f;
     Rigidbody2D rb;
-    GameObject player;
-    WeaponScript weaponScript;
-
+    [SerializeField] Transform target;
+    float movementspeed = 1f;
+    public float nextWayPointDistance = 3f;
+    Vector3 lastTargetPosition = new Vector3(0f,0f,0f);
+    Path path;
+    int currentWaypoint = 0;
+    float reachedWayPointDistance = .4f;
+    bool reachedEndofPath = false;
+    Seeker seeker;
+    float attackRange = 1.3f;
+    float vel;
 
     void Start()
     {
-        currentHealth = maxHealth;
-        rb = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Mario"); //TODO Mario won't be Mario forever
-        weaponScript = GameObject.Find("Mario/Sword").GetComponent<WeaponScript>(); //TODO Don't have damage number in weapon script please (Maybe in GameManager?)
-        
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();       
+        seeker.StartPath(rb.position,target.position,OnPathComplete); 
+        InvokeRepeating("UpdatePath",0f,.5f);
     }
-    //pathfinding etc general behaviour
+    void UpdatePath()
+    {
+        if(lastTargetPosition != target.position)//only if player moved
+        {
+            lastTargetPosition = target.position;
+            if(seeker.IsDone())
+                seeker.StartPath(rb.position,target.position,OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        vel = rb.velocity.magnitude;
+        if(path == null)
+            return;
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            Debug.Log(path.vectorPath.Count + ":"+ currentWaypoint);
+            reachedEndofPath = true;
+            return;
+        }else
+        {
+            reachedEndofPath = false;
+        }
+        if(Vector2.Distance(rb.position,target.position) <= attackRange * 2f/3f){
+            Attack();
+            //Debug.Log("ATTACKRANGE");
+            reachedEndofPath = true;
+        }
+        if(!reachedEndofPath)
+        {
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            Vector2 force = direction * movementspeed;
+            rb.velocity = force;
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            if(distance < reachedWayPointDistance)
+                ++currentWaypoint;
+        }
+    }
+    private void Attack()
+    {
+        //Debug.Log("AttackPlayer");
+        //do something
+    }
 }
