0ae692483dc41260db278713a9947fb8839a5006
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-05-21 14:56:40 +0200

    make barrel hittable but still not working :/

---
+++ Assets/Scripts/HittableBarrel.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class HittableBarrel : HittableObject
+{
+    [SerializeField] private Sprite destroyedFirstSprite;
+    [SerializeField] private Sprite destroyedSecondSprite;
+    [SerializeField] private Sprite destroyedThirdSprite;
+    private SpriteRenderer spriteRenderer;
+    [FormerlySerializedAs("Sound")] public GameObject sound;
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Die()
+    {
+        // Sound
+        sound.GetComponent<RandomSound>().PlayRandom2();
+        //change sprite
+        spriteRenderer.sprite = destroyedThirdSprite;
+        base.Die();
+    }
+    protected override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        if (currentHealth <= maxHealth * 1f / 3f && currentHealth > 0)
+        {
+            spriteRenderer.sprite = destroyedSecondSprite;
+            // Sound
+            sound.GetComponent<RandomSound>().PlayRandom1();
+        }
+        else if (currentHealth <= maxHealth * 2f / 3f && currentHealth > 0)
+        {
+            // Sound
+            sound.GetComponent<RandomSound>().PlayRandom1();
+            spriteRenderer.sprite = destroyedFirstSprite;
+        }
+
+    }
+}
