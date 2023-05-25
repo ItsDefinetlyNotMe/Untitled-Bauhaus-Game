baac8bc8271d8b90787c5075642570423380838d
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-05-25 14:52:50 +0200

    make main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool isLoading;
+
+    public void NewGame()
+    {
+        isLoading = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void LoadGame()
+    {
+        isLoading = true;
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
