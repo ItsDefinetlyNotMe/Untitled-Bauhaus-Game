ca21095e9222cc3127296bb3633e532b2bcb7afd
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-05-29 09:48:19 +0200

    start of spearenemy

--- Assets/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using static Structs;
 using Enemies;
+using Unity.VisualScripting;
 using UnityEngine;
 
 abstract public class MeleeEnemy : EnemyMovement
@@ -19,7 +20,8 @@ abstract public class MeleeEnemy : EnemyMovement
         protected Animator animator; 
 
         [Header("AttackRanges")]
-        [SerializeField]protected float attackRange = 10f;
+        [SerializeField]protected float attackRange = 1f;
+        [SerializeField] protected float aggroRange = 50f;
     
         [Header("AttackTime")]
         [SerializeField] protected float chargeAttackTime = .6f;
@@ -34,17 +36,49 @@ abstract public class MeleeEnemy : EnemyMovement
         /// <summary> Figuring out what to do next based on The state Enemy is in</summary>
         protected void NextMove()
         {//figuring out what to do next called in update
-            if (currentEnemyState == EnemyState.Moving)
+            float distance = Vector2.Distance(transform.position, target.position - new Vector3(0f,0.5f,0f));
+            if (currentEnemyState != EnemyState.Attacking)
             {
-                if (Vector3.Distance(transform.position, target.position) <= attackRange * 0.8f)
+                if (distance <= attackRange * 0.8f)
+                {
                     StartAttack();
+                }
+            }
+
+            if (distance > aggroRange)
+            {
+                StopTargeting();
+                ChangeState(EnemyState.Idle);
+            }
+            else if(currentEnemyState == EnemyState.Idle)
+            {
+                StartTargeting();
             }
         }
         private void StartAttack()
         {
-            //figure out direction
-            Attack(Direction.Down);
 
+            //Ray to look wether there is an obstacle between u and player
+            Vector2 raydirection = (target.position - transform.position).normalized;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, raydirection, attackRange);
+            if(hit)
+                if(!hit.transform.CompareTag("Player"))
+                    return;
+
+            Attack(Direction.Down);
+            
+/*
+            //figure out direction
+            if(raydirection.x > raydirection.y)
+                Attack(Direction.Down);
+            else if (raydirection.x < raydirection.y)
+            {
+                
+            }
+            else if (Mathf.Abs(raydirection.x) > raydirection.y)
+            {
+                
+            }*/
         }
 
         protected virtual void ChangeState(EnemyState nextState)
@@ -53,4 +87,5 @@ abstract public class MeleeEnemy : EnemyMovement
         }
         /// <summary> Needs to be overriden, implements The Attack  </summary>
         protected abstract IEnumerator Attack(Direction direction);
+        
 }
--- Assets/Scripts/Enemies/SpearAndShieldEnemy.cs
+++ Assets/Scripts/Enemies/SpearAndShieldEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Enemies;
@@ -8,22 +9,48 @@ using Random = Unity.Mathematics.Random;
 
 public class SpearAndShieldEnemy : MeleeEnemy
 {
+    private void Start()
+    {
+        StartTargeting();
+        base.StartUp();
+    }
+
+    private void Update()
+    {
+        if (currentEnemyState == EnemyState.Moving || currentEnemyState == EnemyState.Idle)
+        {
+            animator.SetBool("isWalking",rb.velocity.magnitude > 0.1f);
+            //For some reason the rb stops every now and then
+            animator.SetFloat("x",direction.x);
+            animator.SetFloat("y",direction.y);
+        }
+        NextMove();
+    }
+
     protected override IEnumerator Attack(Direction direction)
     {
+
         ChangeState(EnemyState.ChargingAttack);
         int[] attackdirection = new int[3];
         for (int i = 0; i < 3; ++i)
             attackdirection[i] = UnityEngine.Random.Range(1, 4);
         yield return new WaitForSeconds(chargeAttackTime);
-        
+        animator.SetBool("isAttacking", true);
+        animator.SetInteger("AttackDirection",(int)direction);
         // animation will change state to attacking
         foreach (int i in attackdirection)
         {
             //play animation 1..3
-            animator.SetInteger("Attackdirection",i);
+            animator.SetInteger("AttackSmallDirection",i);
             yield return new WaitWhile(() => currentEnemyState == EnemyState.Attacking);
         }
+        animator.SetBool("isAttacking",false);
         //play animation
         yield return new WaitForSeconds(rechargingTime);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position,attackRange);
+        Gizmos.DrawLine(transform.position,transform.position);
     }
 }
