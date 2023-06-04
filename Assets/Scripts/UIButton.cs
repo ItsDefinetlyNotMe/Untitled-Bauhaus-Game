5fbdc7ec043447a404de5352ba0fa3b7aa36561c
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-06-04 15:30:36 +0200

    go on with the skill system

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -5,8 +5,25 @@ using UnityEngine.UI;
 
 public class UIButton : MonoBehaviour
 {
+    private UpgradeWindow upgradeWindow;
+
     public void SelectThisButton()
     {
         GetComponent<Button>().Select();
+    }
+
+    public void IncreaseMaxHealth()
+    {
+        upgradeWindow.IncreaseMaxHealth();
+    }
+
+    public void IncreaseDamageMultiplier()
+    {
+        upgradeWindow.IncreaseDamageMultiplier();
+    }
+
+    private void Awake()
+    {
+        upgradeWindow = FindObjectOfType<UpgradeWindow>();
     }
 }
--- Assets/Scripts/UpgradeWindow.cs
+++ Assets/Scripts/UpgradeWindow.cs
@@ -1,19 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UpgradeWindow : MonoBehaviour
 {
+    private GameManager gameManager;
     private PlayerInput playerInput;
     private InputHandler inputHandler;
     private GameObject upgradeWindow;
 
+    private int maxHealthUpgrade = 10;
+    private int maxHealthBonus = 0;
+    private int maxHealthBasePrice = 0;
+
+    private bool isFirstCall = true;
+
     public void Interact()
     {
+        playerInput.actions.FindActionMap("Movement").Disable();
+
         upgradeWindow.SetActive(true);
 
-        playerInput.actions.FindActionMap("Movement").Disable();
+        isFirstCall = true;
     }
 
     public void Back()
@@ -23,12 +35,66 @@ public class UpgradeWindow : MonoBehaviour
         playerInput.actions.FindActionMap("Movement").Enable();
     }
 
+    public void IncreaseMaxHealth()
+    {
+        if (Input.GetJoystickNames().Count() > 0)
+        {
+            foreach (var kvp in Input.GetJoystickNames())
+                print(kvp);
+
+            if (isFirstCall)
+            {
+                isFirstCall = false;
+                return;
+            }
+        }
+
+        print("Increase Health");
+
+        maxHealthBonus = PlayerPrefs.GetInt("maxHealth" + gameManager.saveSlot) + maxHealthUpgrade; //Get value and add new bonus
+        PlayerPrefs.SetInt("maxHealth" + gameManager.saveSlot, maxHealthBonus); //Set new value
+
+        PlayerPrefs.Save(); //Save changes in playerPrefs
+
+        updateMaxHealth();
+    }
+
+    private void updateMaxHealth()
+    {
+        Transform maxHealthDisplay = transform.GetChild(0).GetChild(1);
+        maxHealthDisplay.GetChild(1).GetComponent<TMP_Text>().text = maxHealthBonus.ToString();
+        TMP_Text text = maxHealthDisplay.GetChild(2).GetChild(0).GetComponent<TMP_Text>();
+
+        int newPrice = maxHealthBasePrice;
+        for (int i = 0; i < maxHealthBonus / 10; i++)
+        {
+            newPrice = (int)(newPrice * 1.1);
+        }
+
+        text.text = newPrice.ToString();
+    }
+
+    public void IncreaseDamageMultiplier()
+    {
+        print("Increase Damage Multiplier");
+    }
+
+    private void Awake()
+    {
+        maxHealthBasePrice = int.Parse(transform.GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetComponent<TMP_Text>().text);
+    }
+
     private void Start()
     {
         inputHandler = FindObjectOfType<InputHandler>();
         playerInput = FindObjectOfType<PlayerInput>();
+        gameManager = FindObjectOfType<GameManager>();
 
         upgradeWindow = transform.GetChild(0).gameObject;
+
+        updateMaxHealth();
+
+        //PlayerPrefs.SetInt("maxHealth" + gameManager.saveSlot, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
