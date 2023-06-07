42f921684afd7dae2598a8f7daa64b2a374f78fe
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-06-07 11:25:08 +0200

    add fireplace destroy animation & deactivate light if they die

--- Assets/Scripts/HittableFireplace.cs
+++ Assets/Scripts/HittableFireplace.cs
@@ -1,24 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.Serialization;
 
 public class HittableFireplace : HittableObject
 {
-    [SerializeField] private Sprite destroyedSprite;
+    private Animator animator;
     SpriteRenderer spriteRenderer;
+    private int stage;
+    private Transform layerRoot;
+    [SerializeField] private Light2D light2D;
+    
 
     protected override void Start()
     {
         base.Start();
+        layerRoot = transform.Find("LayerRoot");
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        
     }
 
+    protected override void TakeDamage(int damage,GameObject damageSource)
+    {
+        base.TakeDamage(damage,damageSource);
+        if (currentHealth <= maxHealth * 1f / 3f && currentHealth > 0 && stage == 0)
+        {
+            stage = 1;
+            //priteRenderer.sprite = destroyedSecondSprite;
+            animator.SetTrigger("nextStage");
+        }
+        else if (currentHealth <= maxHealth * 2f / 3f && currentHealth > 0)
+        {
+            stage = 2;
+            animator.SetTrigger("nextStage");
+
+        }
+
+    }
     protected override void Die(GameObject damageSource)
     {
         //change sprite
-        spriteRenderer.sprite = destroyedSprite;
-
+        //spriteRenderer.sprite = destroyedSprite;
+        animator.SetTrigger("nextStage");
+        light2D.intensity = 0f;
+        //layerRoot.position += Vector3.up * 0.3f;
         base.Die(damageSource);
     }
     //protected override void TakeDamage(int damage)
