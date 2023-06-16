aa9b45d5cbd8306c6b50ddc4684e2fff7f5ac47a
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-06-16 12:39:55 +0200

    Golden boi head rotation

--- Assets/Scripts/Enemies/Head.cs
+++ Assets/Scripts/Enemies/Head.cs
@@ -8,12 +8,28 @@ public class Head : MonoBehaviour
 {
     private CircleCollider2D col;
     private ThrowHeadEnemy throwHeadEnemy;
+    private Rigidbody2D rb;
+    private Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CircleCollider2D>();
         throwHeadEnemy = GetComponentInParent<ThrowHeadEnemy>();
     }
-
+    
+    private void  Update()
+    {
+        string rolldirection = "Left"; 
+        Vector3 dir = rb.velocity.normalized;
+        if (dir.y > 0 && Mathf.Abs(dir.x) <= dir.y)
+            rolldirection =  "Up";
+        else if(Mathf.Abs(dir.x) <= Mathf.Abs(dir.y))
+            rolldirection = "Down";
+        else if (dir.x > 0)
+            rolldirection = "Right";
+        animator.Play("RollingHead" + rolldirection);
+    }
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
         throwHeadEnemy.AttackHit(collision2D);
