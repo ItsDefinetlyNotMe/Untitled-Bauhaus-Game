7f94dcfffdc025d64d23509e4dc274d084e7accd
Raphael <50047718+Nordwindfeld@users.noreply.github.com>
2023-06-21 12:40:09 +0200

    SpearAndShield Sounds

--- Assets/Audio/Sounddesign/Scripts/PlaySound.cs
+++ Assets/Audio/Sounddesign/Scripts/PlaySound.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     public AudioSource randomSound1;
+    public AudioSource randomSound2;
+    public AudioSource randomSound3;
+    public AudioSource randomSound4;
+
     void playSound() 
     {
         randomSound1.Play();
+    }
+
+    void playSound2()
+    {
+        randomSound2.Play();
+    }
+
+    void playSound3()
+    {
+        randomSound3.Play();
+    }
+
+    void playSound4()
+    {
+        randomSound4.Play();
     }
 }
