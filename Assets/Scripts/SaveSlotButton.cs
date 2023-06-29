c83fb292434eabe48bc03492b609ce47e70c0da6
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-06-29 16:50:41 +0200

    add save slot description

---
+++ Assets/Scripts/SaveSlotButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSlotButton : MonoBehaviour
+{
+    [SerializeField] private int slotNumber;
+
+    private void OnEnable()
+    {
+        
+    }
+}
