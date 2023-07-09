2194651d5ea502929f7397582c8e9ddf84bf15c8
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-07-09 13:28:07 +0200

    The Head doth now not reave thee from the grave

--- Assets/Scripts/Enemies/HittableHead.cs
+++ Assets/Scripts/Enemies/HittableHead.cs
@@ -8,10 +8,12 @@ public class HittableHead : HittableEnemy
     // Start is called before the first frame update
     HittableEnemy parentHittableScript;
     private SpriteRenderer spriteRendererHead;
+    private Collider2D col;
     protected override void Start()
     {
         parentHittableScript = transform.parent.GetComponent<HittableEnemy>();
         spriteRendererHead = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
     }
 
     protected override void Die(GameObject damageSource)
@@ -21,8 +23,9 @@ public class HittableHead : HittableEnemy
 
     public override void GetHit(int damage, Vector2 damageSourcePosition, float knockbackMultiplier, GameObject damageSource, bool heavy)
     {
-        parentHittableScript.GetHit(9999,damageSourcePosition,0,damageSource,heavy);
         spriteRendererHead.enabled = false;
+        col.enabled = false;
+        parentHittableScript.GetHit(9999,damageSourcePosition,0,damageSource,heavy);
 
     }
 }
