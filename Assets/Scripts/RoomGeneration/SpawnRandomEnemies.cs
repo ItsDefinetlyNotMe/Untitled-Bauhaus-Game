35027347971a89d037d045433c414d8406a8f8f0
Nordwindfeld <raphaelhuerler@yahoo.de>
2023-07-10 10:13:32 +0200

    Scenes are back in the biz +  Winning Sound

--- Assets/Scripts/RoomGeneration/SpawnRandomEnemies.cs
+++ Assets/Scripts/RoomGeneration/SpawnRandomEnemies.cs
@@ -31,6 +31,8 @@ namespace TestRandomWorldGeneration {
 
         private bool isFirstWave = true;
 
+        public GameObject WinningSound;
+
         public void StartEnemySpawning(ref float[,] newTileMatrix, int newFloorTileCount, int remainingSpace, int restDifficulty)
         {
             ResetEverything();
@@ -116,11 +118,11 @@ namespace TestRandomWorldGeneration {
 
             if (livingEnemyCounter <= 0)
             {
+                WinningSound.GetComponent<PlaySound>().playSound();
                 createRandomRoomLayout.SpawnCollectable();
                 livingEnemyCounter = 0;
-
-                GameObject PostProcess = GameObject.FindGameObjectWithTag("Volume");
-                PostProcess.GetComponent<PostProcessEffects>().GoldVignette(2f);
+                //GameObject PostProcess = GameObject.FindGameObjectWithTag("Volume");
+                //PostProcess.GetComponent<PostProcessEffects>().GoldVignette(2f);
             }
         }
 
