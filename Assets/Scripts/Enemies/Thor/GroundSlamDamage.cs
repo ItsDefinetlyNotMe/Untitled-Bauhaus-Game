f28b381f27751857b454dff5809eb293a17979a9
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-07-12 17:42:49 +0200

    implement getting hit from ground slam

---
+++ Assets/Scripts/Enemies/Thor/GroundSlamDamage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSlamDamage : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            other.transform.GetComponent<HitablePlayer>().GetHit(50, transform.parent.position, 20f, transform.parent.gameObject, false);
+    }
+}
